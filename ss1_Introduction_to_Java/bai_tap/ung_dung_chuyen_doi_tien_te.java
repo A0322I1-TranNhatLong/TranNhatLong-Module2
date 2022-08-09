b78d893323d6c42b5be20d99c34167a7b92fe8bb
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-08-09 11:03:45 +0700

    upload

---
+++ ss1_Introduction_to_Java/bai_tap/ung_dung_chuyen_doi_tien_te.java
@@ -0,0 +1,15 @@
+package ss1_Introduction_to_Java.bai_tap;
+
+import java.util.Scanner;
+
+public class ung_dung_chuyen_doi_tien_te {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mời bạn nhập số tiền USD: ");
+        usd = sc.nextDouble();
+        double quydoi = usd * 23000;
+        System.out.print("Giá trị VNĐ: " + quydoi);
+    }
+}
