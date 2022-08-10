f998ce6f944a2a3619f9dcb924fde1d617c909a1
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-08-10 18:43:47 +0700

    upload

---
+++ ss2_vong_lap_trong_java/bai_tap/hien_thi_cac_so_nguyen_to_nho_hon_100.java
@@ -0,0 +1,32 @@
+package ss2_vong_lap_trong_java.bai_tap;
+
+public class hien_thi_cac_so_nguyen_to_nho_hon_100 {
+    public static void main(String[] args){
+        int N=2;
+        while(N<=100)
+        {
+            if(CheckSNT(N)==1)
+            {
+                System.out.println(N);
+            }
+            N++;
+        }
+    }
+    public static int CheckSNT(int so) {
+        int d = 0;
+        if (so < 2) {
+            return 0;
+        } else {
+            for (int i = 1; i <= so; i++) {
+                if (so % i == 0) {
+                    d++;
+                }
+            }
+            if (d == 2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
