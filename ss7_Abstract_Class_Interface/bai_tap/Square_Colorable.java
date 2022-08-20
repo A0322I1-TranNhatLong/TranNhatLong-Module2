5ba0f98c6092745f9c3cf26d60d290e72c6412e2
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-08-20 18:26:13 +0700

    upload

---
+++ ss7_Abstract_Class_Interface/bai_tap/Square_Colorable.java
@@ -0,0 +1,16 @@
+package ss7_Abstract_Class_Interface.bai_tap;
+
+public class Square_Colorable extends Square implements Colorable{
+    @Override
+    public void howToColor() {
+        System.out.println(" Color all four sides...");
+    }
+    public Square_Colorable() {
+    }
+    public Square_Colorable(double side) {
+        super(side);
+    }
+    public Square_Colorable(double side, String color, boolean filled) {
+        super(side, color, filled);
+    }
+}
