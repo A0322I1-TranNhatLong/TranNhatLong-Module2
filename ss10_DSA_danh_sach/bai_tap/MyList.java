d3f754746ac6f3efefbe9584ed5bfeb8ac0a57fc
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-08-26 19:30:23 +0700

    upload

--- ss10_DSA_danh_sach/bai_tap/MyList.java
+++ ss10_DSA_danh_sach/bai_tap/MyList.java
@@ -6,11 +6,9 @@ public class MyList<E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
-
     public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
     }
-
     public MyList(int capacity) {
         elements = new Object[capacity];
     }
@@ -18,23 +16,18 @@ public class MyList<E> {
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
-
     public void add(E e) {
         if (size == elements.length) {
             ensureCapa();
         }
         elements[size++] = e;
     }
-
     public void add(int index, E e) {
         ensureCapacity();
         for (int i = size - 1; i >= index; i--) elements[i + 1] = elements[i];
         elements[index] = e;
-
         size++;
     }
-
-
     public int size()
     {
         return size;
