fda58b14be5d94455a69c250f2a98714c211b5d0
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-08-26 18:14:47 +0700

    upload

---
+++ ss10_DSA_danh_sach/bai_tap/TestMyList.java
@@ -0,0 +1,23 @@
+package ss10_DSA_danh_sach.bai_tap;
+
+public class TestMyList {
+    public static void main(String[] args) {
+        MyList<String> myList=new MyList<>();
+        myList.add("DaNang");
+        myList.add("HaNoi");
+        myList.add("SaiGon");
+        myList.add("NhaTrang");
+        myList.add("GiaLai");
+        System.out.println(myList.size());
+        for (int i=0; i<myList.size(); i++)
+        {
+            System.out.print(myList.get(i)+" ");
+        }
+        System.out.println();
+        System.out.println("Đã xóa: "+myList.remove(2));
+        for (int i=0; i<myList.size(); i++)
+        {
+            System.out.print(myList.get(i)+" ");
+        }
+    }
+}
