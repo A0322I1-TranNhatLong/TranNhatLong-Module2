6d5360776f4a7fe0342fefe01ec11406bde04c88
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-09-05 18:08:20 +0700

    upload

---
+++ bai_tap_them/bai_tap_1/StudentTest.java
@@ -0,0 +1,2 @@
+package bai_tap_them.bai_tap_1;public class StudentTest {
+}
---
+++ ss11_DSA_stack_queue/bai_tap/dem_so_lan_xuat_hien_tu.java
@@ -0,0 +1,22 @@
+package ss11_DSA_stack_queue.bai_tap;
+
+import java.util.TreeMap;
+
+public class dem_so_lan_xuat_hien_tu {
+        public static void main(String[] args) {
+            TreeMap<String,Integer> treeMapTu=new TreeMap<>();
+            String str="I love CodeGym very much much ";
+            String[] arrayStrings=str.split(" ");
+            for(int i=0; i<arrayStrings.length; i++)
+            {
+                if(treeMapTu.containsKey(arrayStrings[i]))
+                {
+                    int newValue=treeMapTu.get(arrayStrings[i]);
+                    treeMapTu.put(arrayStrings[i],++newValue);
+                }else {
+                    treeMapTu.put(arrayStrings[i],1);
+                }
+            }
+            System.out.println(treeMapTu);
+        }
+}
