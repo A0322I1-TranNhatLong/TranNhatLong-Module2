0841102466a55933df1473d758130c65684e9124
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-09-09 14:19:30 +0700

    upload

--- bai_tap_them/bai_tap_1/Person.java
+++
@@ -1,2 +0,0 @@
-package bai_tap_them.bai_tap_1;public class Person {
-}
---
+++ ss11_DSA_stack_queue/bai_tap/Optional_stack_nhiphan.java
@@ -0,0 +1,21 @@
+package ss11_DSA_stack_queue.bai_tap;
+
+import java.util.Stack;
+
+public class Optional_stack_nhiphan {
+    public static void main(String[] args) {
+        Stack<Integer> stackInteger=new Stack<>();
+        int numberThapPhan=111;
+        int copyNumber=numberThapPhan;
+        while (copyNumber>0)
+        {
+            stackInteger.push(copyNumber%2);
+            copyNumber/=2;
+        }
+        System.out.print(numberThapPhan+" có hệ nhị phân: ");
+        while (!stackInteger.empty())
+        {
+            System.out.print(stackInteger.pop());
+        }
+    }
+}
