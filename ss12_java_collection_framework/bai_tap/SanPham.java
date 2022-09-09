973ee20bde41f125b2a32bca909eea08ee331526
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-09-09 14:35:57 +0700

    upload

---
+++ ss12_java_collection_framework/bai_tap/SanPham.java
@@ -0,0 +1,40 @@
+package ss12_java_collection_framework.bai_tap;
+
+public class SanPham {
+    private String tenSanPham;
+    private String idSanPham;
+    private double giaSanPham;
+    public SanPham() {
+    }
+    public SanPham(String idSanPham, String tenSanPham, double giaSanPham) {
+        this.idSanPham = idSanPham;
+        this.tenSanPham = tenSanPham;
+        this.giaSanPham = giaSanPham;
+    }
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
+    public String getIdSanPham() {
+        return idSanPham;
+    }
+    public void setIdSanPham(String idSanPham) {
+        this.idSanPham = idSanPham;
+    }
+    public double getGiaSanPham() {
+        return giaSanPham;
+    }
+    public void setGiaSanPham(double giaSanPham) {
+        this.giaSanPham = giaSanPham;
+    }
+    @Override
+    public String toString() {
+        return "SanPham{" +
+                "idSanPham='" + idSanPham + '\'' +
+                ", tenSanPham='" + tenSanPham + '\'' +
+                ", giaSanPham=" + giaSanPham +
+                '}';
+    }
+}
