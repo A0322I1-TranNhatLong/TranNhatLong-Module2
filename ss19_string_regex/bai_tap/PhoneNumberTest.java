850054afb8105fed4bb1bbb9baeae4992a332a23
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-09-30 18:09:24 +0700

    upload

---
+++ ss19_string_regex/bai_tap/PhoneNumberTest.java
@@ -0,0 +1,12 @@
+package ss19_string_regex.bai_tap;
+
+public class PhoneNumberTest {
+    public static final String[] PhoneNumbers = new String[] { "84-0978489648", "84-5978489648", "74-0978489648"};
+    public static void main(String[] args) {
+        RegexPhoneNumber regex=new RegexPhoneNumber();
+        for (String string: PhoneNumbers)
+        {
+            System.out.println("Phone number "+string+" is "+regex.validate(string));
+        }
+    }
+}
