c55e55e1a00cef3335b00416df60c0f482b89217
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-10-19 19:35:21 +0700

    upload

--- ss2_vong_lap_trong_java/bai_tap/HienThi20SoNguyenToDauTien.java
+++ ss2_vong_lap_trong_java/bai_tap/HienThi20SoNguyenToDauTien.java
@@ -1,8 +1,6 @@
 package ss2_vong_lap_trong_java.bai_tap;
 
-import java.util.Scanner;
-
-public class hien_thi_20_so_nguyen_to_dau_tien {
+public class HienThi20SoNguyenToDauTien {
     public static void main(String[] args) {
         int numbers ,count =0;
         int so = 1;
--- ss2_vong_lap_trong_java/bai_tap/HienThiCacSoNguyenToNhoHon100.java
+++ ss2_vong_lap_trong_java/bai_tap/HienThiCacSoNguyenToNhoHon100.java
@@ -1,6 +1,6 @@
 package ss2_vong_lap_trong_java.bai_tap;
 
-public class hien_thi_cac_so_nguyen_to_nho_hon_100 {
+public class HienThiCacSoNguyenToNhoHon100 {
     public static void main(String[] args){
         int N=2;
         while(N<=100)
