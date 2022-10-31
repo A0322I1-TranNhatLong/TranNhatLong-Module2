918311b3ef5c607c78c6e6980cb4d24418334e0d
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2022-10-31 18:28:44 +0700

    upload

--- CaseStudy/models/Booking.java
+++ CaseStudy/models/Booking.java
@@ -1,2 +1,108 @@
-package CaseStudy.models;public class Booking {
+package CaseStudy.models;
+
+import CaseStudy.utils.DateException;
+
+import java.util.Date;
+
+public class Booking implements Comparable<Booking> {
+    private int bookingCode;
+    private Date firstDate;
+    private Date lastDate;
+    private int customerCode;
+    private String facilityName;
+    private String facilityType;
+
+    public Booking() {
+    }
+
+    public Booking(int bookingCode, Date firstDate, Date lastDate, int customerCode, String facilityName, String facilityType) {
+        this.bookingCode = bookingCode;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
+        this.customerCode = customerCode;
+        this.facilityName = facilityName;
+        this.facilityType = facilityType;
+    }
+
+    public int getBookingCode() {
+        return bookingCode;
+    }
+
+    public void setBookingCode(int bookingCode) {
+        this.bookingCode = bookingCode;
+    }
+
+    public Date getFirstDate() {
+        return firstDate;
+    }
+
+    public void setFirstDate(Date firstDate) {
+        this.firstDate = firstDate;
+    }
+
+    public Date getLastDate() {
+        return lastDate;
+    }
+
+    public void setLastDate(Date lastDate) {
+        this.lastDate = lastDate;
+    }
+
+    public int getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(int customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getFacilityName() {
+        return facilityName;
+    }
+
+    public void setFacilityName(String facilityName) {
+        this.facilityName = facilityName;
+    }
+
+    public String getFacilityType() {
+        return facilityType;
+    }
+
+    public void setFacilityType(String facilityType) {
+        this.facilityType = facilityType;
+    }
+
+    public String getDay()
+    {
+        return DateException.simpleDateFormatDay.format(firstDate);
+    }
+
+    public String getMonth()
+    {
+        return DateException.simpleDateFormatMonth.format(firstDate);
+    }
+
+    public String getYear()
+    {
+        return DateException.simpleDateFormatYear.format(firstDate);
+    }
+    @Override
+    public String toString() {
+        return  bookingCode +
+                "," + DateException.simpleDateFormat.format(firstDate) +
+                "," + DateException.simpleDateFormat.format(lastDate) +
+                "," + customerCode +
+                "," + facilityName +
+                "," + facilityType;
+    }
+    public String toInfomation()
+    {
+        return   "ID:"+bookingCode+", First day:"+DateException.simpleDateFormat.format(firstDate)
+                +", End day:"+DateException.simpleDateFormat.format(lastDate)+", Customer code:"+customerCode
+                +", Facility: "+facilityName+"-"+facilityType;
+    }
+    @Override
+    public int compareTo(Booking o) {
+        return this.getFirstDate().compareTo(o.getFirstDate());
+    }
 }
--- CaseStudy/models/facitily/Villa.java
+++ CaseStudy/models/facitily/Villa.java
@@ -1,2 +1,63 @@
-package CaseStudy.models.facitily;public class Villa {
+package CaseStudy.models.facitily;
+
+public class Villa extends Facility{
+    private String villaStandard;
+    private double swimPoolArea;
+    private int numberOfFloors;
+    public Villa() {
+    }
+    public Villa(String villaStandard, double swimPoolArea, int numberOfFloors) {
+        this.villaStandard = villaStandard;
+        this.swimPoolArea = swimPoolArea;
+        this.numberOfFloors = numberOfFloors;
+    }
+    public Villa(String tenDichVu, double dienTich, int chiPhiThue, int soNguoiToiDa, String kieuThue, String villaStandard, double swimPoolArea, int numberOfFloors) {
+        super(tenDichVu, dienTich, chiPhiThue, soNguoiToiDa, kieuThue);
+        this.villaStandard = villaStandard;
+        this.swimPoolArea = swimPoolArea;
+        this.numberOfFloors = numberOfFloors;
+    }
+    public Villa(String idDichVu, String tenDichVu, double dienTichSuDung, int chiPhiThue, int soNguoiToiDa, String kieuThue, String villaStandard, double swimPoolArea, int numberOfFloors) {
+        super(idDichVu, tenDichVu, dienTichSuDung, chiPhiThue, soNguoiToiDa, kieuThue);
+        this.villaStandard = villaStandard;
+        this.swimPoolArea = swimPoolArea;
+        this.numberOfFloors = numberOfFloors;
+    }
+    public String getVillaStandard() {
+        return villaStandard;
+    }
+    public void setVillaStandard(String villaStandard) {
+        this.villaStandard = villaStandard;
+    }
+    public double getSwimPoolArea() {
+        return swimPoolArea;
+    }
+    public void setSwimPoolArea(double swimPoolArea) {
+        this.swimPoolArea = swimPoolArea;
+    }
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+    public String toInfomation()
+    {
+        return   "ID:"+facilityID+", Facility:"+facilityName+", Swimming area:"+swimPoolArea
+                + ", Number of floors:"+numberOfFloors+", Villa standard:"+villaStandard+", Usable area:"+usableArea
+                +", Rentalc costs:"+rentalCosts+", Max person:" +numberMaxPerson
+                + ", Rental type:"+rentalType;
+    }
+    @Override
+    public String toString() {
+        return  facilityID +
+                "," + facilityName +
+                "," + swimPoolArea +
+                "," + numberOfFloors +
+                "," + villaStandard +
+                "," + usableArea +
+                "," + rentalCosts +
+                "," + numberMaxPerson +
+                "," + rentalType;
+    }
 }
